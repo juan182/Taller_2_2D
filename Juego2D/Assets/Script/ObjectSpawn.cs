a016ea9f31ccb0d1c66c5d803222e70475d52ad9
YenMur <yennifer.murillo@uao.edu.co>
2025-04-09 22:28:22 -0500

    Cambios prefabs

--- Juego2D/Assets/Script/ObjectSpawn.cs
+++ Juego2D/Assets/Script/ObjectSpawn.cs
@@ -11,6 +11,8 @@ public class ObjectSpawn : MonoBehaviour
     public GameObject AppleSpawn;
     public int numObjSpawn = 5;
 
+    
+
     // Start is called before the first frame update
     void Start()
     {
