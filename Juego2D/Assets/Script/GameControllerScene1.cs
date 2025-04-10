d6bf667897c03a348ed2f17302e6a1bbf4e6116e
juan182 <60664888+juan182@users.noreply.github.com>
2025-04-10 17:44:01 -0500

    PowerUp

--- Juego2D/Assets/Script/GameControllerScene1.cs
+++ Juego2D/Assets/Script/GameControllerScene1.cs
@@ -21,6 +21,7 @@ public class GameControllerScene1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        gate.SetActive(false);
     }
     // Update is called once per frame
     void Update()
